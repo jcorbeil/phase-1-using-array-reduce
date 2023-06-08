d857f7490a695318d8273c5c9965707f830cea9c
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-08 04:37:59 -0400

    finsihed-phase-1-using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function batteries(arr) {
+  return arr.reduce((total, assembly) => total + assembly, 0);
+}
+
+const totalBatteries = batteries(batteryBatches);
+console.log(totalBatteries); // Output: 31
